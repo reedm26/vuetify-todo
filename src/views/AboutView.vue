1c6acbf215098994d64523ab0e1685fddf9f4cad
Marcel Reed <reedm26@yahoo.com>
2022-06-02 14:40:58 -0600

    add filler on about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todos</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, alias aliquid architecto dolorem dolores, ducimus
+      exercitationem inventore, iste libero minus perferendis quibusdam. Animi ea minus nostrum porro quis! Ad,
+      praesentium!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias debitis, doloremque eaque explicabo minima
+      necessitatibus, nobis possimus quos ratione reiciendis sequi suscipit tempora velit voluptas voluptatem! Ad
+      laborum officia perspiciatis!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias debitis, doloremque eaque explicabo minima
+      necessitatibus, nobis possimus quos ratione reiciendis sequi suscipit tempora velit voluptas voluptatem! Ad
+      laborum officia perspiciatis!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias debitis, doloremque eaque explicabo minima
+      necessitatibus, nobis possimus quos ratione reiciendis sequi suscipit tempora velit voluptas voluptatem! Ad
+      laborum officia perspiciatis!</p>
   </div>
 </template>
