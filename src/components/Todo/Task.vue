ae37ab84b3d996dfcd90c207cee196e57c961b2a
Marcel Reed <reedm26@yahoo.com>
2022-06-03 12:24:41 -0600

    add template for dialog

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -25,12 +25,17 @@
       </template>
     </v-list-item>
     <v-divider></v-divider>
+
+    <dialog-delete/>
   </div>
 </template>
 
 <script>
 export default {
   name: "Task",
+  components: {
+    'dialog-delete': require('@/components/Todo/Dialogs/DialogDelete').default,
+  },
   props: ['task']
 }
 </script>
