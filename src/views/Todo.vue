cebda217a0cdb24f8e4adffc786040b203d4dc1c
Marcel Reed <reedm26@yahoo.com>
2022-06-03 00:54:58 -0600

    add checkboxes and some data for tasks

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,52 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo Page</h1>
+  <div class="home">
+    <v-list
+        flat
+       class="pt-0"
+    >
+      <div  v-for="task in tasks" :key="task.id">
+        <v-list-item>
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
   </div>
 </template>
 
 <script>
 
-  export default {
-    name: 'Home',
-
-    components: {
-    },
+export default {
+  name: 'Home',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Wake Up',
+          done: false,
+        },
+        {
+          id: 2,
+          title: 'Do laundry',
+          done: false,
+        },
+        {
+          id: 3,
+          title: 'Walk dog',
+          done: false,
+        }
+      ]
+    }
   }
+
+}
 </script>
